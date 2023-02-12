cc7aa59f86aee9f699a3996120d48161e6868a44
bohurtadop <bohurtadop@unal.edu.co>
2023-02-12 03:09:57 -0500

    Show companyName in Thanks page

--- src/pages/Thanks.js
+++ src/pages/Thanks.js
@@ -3,12 +3,15 @@ import './styles.css';
 import FeelingFooter from '../components/FeelingFooter';
 import QuestionsHeader from '../components/FeelingHeader';
 import ThanksContainer from '../components/ThanksContainer';
+import { useLocation } from 'react-router-dom';
 
 function Thanks() {
 
+    const location = useLocation();
+
     return (
         <div className='Content'>
-            <QuestionsHeader />
+            <QuestionsHeader companyName={location.state.companyName} />
             <br />
             <ThanksContainer />
             <FeelingFooter />
